11c2085b741cb1984979ed2dabfe98766530b563
zabdielcead <zabdiel.cead@gmail.com>
2021-09-14 20:51:13 -0500

    swagger

--- src/main.ts
+++ src/main.ts
@@ -30,8 +30,8 @@ async function bootstrap() {
         .build();
       const document = SwaggerModule.createDocument(app, config);
       SwaggerModule.setup('api', app, document);
-    
-      await app.listen(configService.get<number>('port'), configService.get<string>('host'));
+      await app.listen(3000);
+      //await app.listen(configService.get<number>('port'), configService.get<string>('host'));
     
 }
 bootstrap();
